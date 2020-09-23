83dcdfa6b9b1849d470487f251918b6ffa9f8de6
mar156 <m.n.v_156@hotmail.com.ar>
2020-09-23 09:11:09 -0300

    Agregué antecedentes

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, ()=> console.log("El servidor funciona bien"));
+
+app.get('/', (req,res )=>
+{
+    res.sendFile(__dirname + "/tesis.html")
+})
